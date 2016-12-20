0ce4cb9bb6359b906cfdfd33906e155e53957302
jason <jason.bunk@gmail.com>
2016-12-19 23:19:05 -0800

    TestOpenCVPreview layer to visualize intermediate tensors

---
+++ include/caffe/layers/testopencvpreview_layer.hpp
@@ -0,0 +1,51 @@
+#ifndef CAFFE_TESTOPENCVPREVIEWIMGS_LAYER_HPP_
+#define CAFFE_TESTOPENCVPREVIEWIMGS_LAYER_HPP_
+
+#include <vector>
+
+#include "caffe/blob.hpp"
+#include "caffe/layer.hpp"
+#include "caffe/proto/caffe.pb.h"
+
+#include "caffe/layers/neuron_layer.hpp"
+
+namespace caffe {
+
+/**
+ * @brief Previews images in batch using opencv; if many channels (>3), will visualize the first 3 channels as RGB.
+ *
+ * @param bottom input Blob vector (length 1)
+ *   -# @f$ (N \times C \times H \times W) @f$
+ *      the inputs @f$ x @f$
+ * @param top output Blob vector (length 1)
+ *   -# @f$ (N \times C \times H \times W) @f$
+ *      the copied inputs @f$ y = x @f$
+ */
+template <typename Dtype>
+class TestOpenCVPreviewLayer : public NeuronLayer<Dtype> {
+ public:
+  explicit TestOpenCVPreviewLayer(const LayerParameter& param)
+      : NeuronLayer<Dtype>(param) {}
+  virtual void LayerSetUp(const vector<Blob<Dtype>*>& bottom,
+      const vector<Blob<Dtype>*>& top);
+
+  virtual inline const char* type() const { return "TestOpenCVPreview"; }
+  virtual inline int ExactNumBottomBlobs() const { return 1; }
+  virtual inline int ExactNumTopBlobs() const { return 1; }
+
+ protected:
+  /// @copydoc TestOpenCVPreviewLayer
+  virtual void Forward_cpu(const vector<Blob<Dtype>*>& bottom,
+      const vector<Blob<Dtype>*>& top);
+  //virtual void Forward_gpu(const vector<Blob<Dtype>*>& bottom,
+  //    const vector<Blob<Dtype>*>& top);
+
+  virtual void Backward_cpu(const vector<Blob<Dtype>*>& top,
+      const vector<bool>& propagate_down, const vector<Blob<Dtype>*>& bottom);
+  //virtual void Backward_gpu(const vector<Blob<Dtype>*>& top,
+  //    const vector<bool>& propagate_down, const vector<Blob<Dtype>*>& bottom);
+};
+
+}  // namespace caffe
+
+#endif  // CAFFE_TESTOPENCVPREVIEWIMGS_LAYER_HPP_
---
+++ src/caffe/layers/testopencvpreview_layer.cpp
@@ -0,0 +1,72 @@
+#include <vector>
+
+#include "caffe/layers/testopencvpreview_layer.hpp"
+#include "caffe/util/math_functions.hpp"
+#include <opencv2/imgproc/imgproc.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#define MINOF2(x,y) ((x)<(y)?(x):(y))
+#define MAXOF2(x,y) ((x)>(y)?(x):(y))
+
+namespace caffe {
+
+template <typename Dtype>
+void TestOpenCVPreviewLayer<Dtype>::LayerSetUp(const vector<Blob<Dtype>*>& bottom,
+      const vector<Blob<Dtype>*>& top) {
+  NeuronLayer<Dtype>::LayerSetUp(bottom, top);
+  CHECK_NE(top[0], bottom[0]) << this->type() << " Layer does not allow in-place computation.";
+}
+
+template <typename Dtype>
+void TestOpenCVPreviewLayer<Dtype>::Forward_cpu(
+    const vector<Blob<Dtype>*>& bottom, const vector<Blob<Dtype>*>& top) {
+  const Dtype* bottom_data = bottom[0]->cpu_data();
+  caffe_copy(bottom[0]->count(), bottom_data, top[0]->mutable_cpu_data());
+
+  CHECK_EQ(bottom[0]->num_axes(), 4);
+  const int nbatch = bottom[0]->shape(0);
+  const int nchans = MINOF2(bottom[0]->shape(1),3);
+  const int nrows = bottom[0]->shape(2);
+  const int ncols = bottom[0]->shape(3);
+  const int bytesperchan = ncols*nrows*sizeof(Dtype);
+  std::vector< cv::Mat_<Dtype> > testimgchans;
+  for(int cc=0; cc<nchans; ++cc) {
+    testimgchans.push_back(cv::Mat_<Dtype>(nrows, ncols));
+  }
+  cv::Mat mergedmat;
+  double minval,maxval,globalmin,globalmax;
+  globalmin = 1e20; globalmax = -1e20;
+
+  for(int mm=0; mm<nbatch; ++mm) {
+    for(int cc=0; cc<nchans; ++cc) {
+      memcpy(testimgchans[cc].data,
+             bottom_data + bottom[0]->offset(mm, cc, 0, 0),
+             bytesperchan);
+      cv::minMaxIdx(testimgchans[cc], &minval, &maxval);
+      globalmin = MINOF2(globalmin, minval);
+      globalmax = MAXOF2(globalmax, maxval);
+    }
+    cv::merge(testimgchans, mergedmat);
+    cv::imshow("caffe-img", (mergedmat-globalmin)/(globalmax-globalmin));
+    cv::waitKey(0);
+  }
+}
+
+template <typename Dtype>
+void TestOpenCVPreviewLayer<Dtype>::Backward_cpu(const vector<Blob<Dtype>*>& top,
+    const vector<bool>& propagate_down, const vector<Blob<Dtype>*>& bottom) {
+  const int count = top[0]->count();
+  const Dtype* top_diff = top[0]->cpu_diff();
+  if (propagate_down[0]) {
+    Dtype* bottom_diff = bottom[0]->mutable_cpu_diff();
+    caffe_copy(count, top_diff, bottom_diff);
+  }
+}
+
+#ifdef CPU_ONLY
+STUB_GPU(TestOpenCVPreviewLayer);
+#endif
+
+INSTANTIATE_CLASS(TestOpenCVPreviewLayer);
+REGISTER_LAYER_CLASS(TestOpenCVPreview);
+
+}  // namespace caffe
